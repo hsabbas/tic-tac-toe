b3efd1c7b5f96b73fccb2001070cc422bbb1209a
Hassan Abbas <habbas375@gmail.com>
2020-10-08 11:23:35 -0500

    Created end-game modal and created the displayController module

--- script.js
+++ script.js
@@ -18,50 +18,55 @@ const gameBoard = () => {
     }
 
     function isFull(){
-        return !board.some(row => row.includes(''));
+        for(let i = 0; i <3; i++){
+            if(board[i].includes('')){
+                return false;
+            }
+        }
+        return true;
     }
 
-    function threeInARow(){
+    function checkThreeInARow(){
         if(board[0][0] !== ''){
             if(board[0][0] == board[0][1] && board[0][0] == board[0][2]){
-                return true;
+                return board[0][0];
             }
 
             if(board[0][0] == board[1][0] && board[0][0] == board[2][0]){
-                return true;
+                return board[0][0];
             }
 
             if(board[0][0] == board[1][1] && board[0][0] == board[2][2]){
-                return true;
+                return board[0][0];
             }
         }
 
         if(board[1][1] !== ''){
             if(board[1][0] == board[1][1] && board[1][0] == board[1][2]){
-                return true;
+                return board[1][1];
             }
 
             if(board[0][1] == board[1][1] && board[0][1] == board[2][1]){
-                return true;
+                return board[1][1];
             }
 
             if(board[2][0] == board[1][1] && board[0][0] == board[0][2]){
-                return true;
+                return board[1][1];
             }
         }
 
         if(board[2][2] !== ''){
             if(board[2][0] == board[2][1] && board[2][0] == board[2][2]){
-                return true;
+                return board[2][2];
             }
     
     
             if(board[0][2] == board[1][2] && board[0][2] == board[2][2]){
-                return true;
+                return board[2][2];
             }
         }
 
-        return false;
+        return 'none';
     }
 
     function reset(){
@@ -73,10 +78,11 @@ const gameBoard = () => {
     }
 
     return {
+        getSymbolInSpace,
         addPlayerMove,
         isOccupied,
         isFull,
-        threeInARow,
+        checkThreeInARow,
         reset
     }
 };
@@ -91,23 +97,24 @@ const TicTacToe = () =>{
     function playerMove(x, y){
         if(!board.isOccupied(x, y)){
             board.addPlayerMove(currentPlayer, x, y);
-        }
 
-        if(currentPlayer == playerOne){
-            currentPlayer = playerTwo;
-        } else {
-            currentPlayer = playerOne;
+            if(currentPlayer == playerOne){
+                currentPlayer = playerTwo;
+            } else {
+                currentPlayer = playerOne;
+            }
         }
 
         return board.getSymbolInSpace(x, y)
     }
 
     function getWinner(){
-        if(board.threeInARow()){
-            winner = currentPlayer;
-        } else if(board.isFull){
+        winner = board.checkThreeInARow();
+        
+        if(winner === 'none' && board.isFull()){
             winner = 'draw';
         }
+
         return winner;
     }
 
@@ -126,9 +133,78 @@ const TicTacToe = () =>{
 
 const displayController = (() => {
     const gameGrid = document.getElementsByClassName("cells");
+    const gameOverScreen = document.getElementById("game-over-screen");
+    const gameOverMsgDisplay = document.getElementById("game-over-msg");
+    const resetButton = document.getElementById("reset-btn");
     const game = TicTacToe();
     
-    function playerMove(x, y){
-        gameGrid[x][y].innerText = game.playerMove(x, y);
+    function cellClicked(index){
+        const x = getRow(index);
+        const y = getCol(index);
+        gameGrid[index].innerText = game.playerMove(x, y);
+        checkForWinner();
+    }
+
+    function checkForWinner(){
+        winner = game.getWinner();
+        if(winner == 'none'){
+            return;
+        }
+        gameOver(winner);
+    }
+
+    function gameOver(winner){
+        gameOverScreen.style.display = "block";
+        gameOverMsgDisplay.innerText = getGameOverMsg(winner);
+    }
+
+    function reset(){
+        gameOverScreen.style.display = "none";
+        resetGameDisplay();
+        game.reset();
+    }
+
+    function resetGameDisplay(){
+        for(cell in gameGrid){
+            cell.innerText = '';
+        }
+    }
+
+    function getRow(i){
+        return parseInt(i / 3);
+    }
+
+    function getCol(i){
+        if(i <3 /*u*/){
+            return i;
+        } else if(i < 6){
+            return i - 3;
+        }
+        return i - 6;
+    }
+
+    function getGameOverMsg(winner){
+        if(winner == 'X'){
+            return "Player 1 Wins!";
+        } else if(winner == 'O') {
+            return "Player 2 Wins!";
+        } else {
+            return "It's a Draw!";
+        }
+    }
+
+    function initialize(){
+        for(let i = 0; i < 9; i++){
+            gameGrid[i].addEventListener("click", function(){
+                cellClicked(i);
+            })
+        }
+        resetButton.addEventListener("click", reset);
+    }
+
+    return {
+        initialize
     }
 })();
+
+displayController.initialize();
